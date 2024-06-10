6041f32920c5036b7e5580dcb9660fa5c2537550
Hayley Smith <158859106+justhaylz@users.noreply.github.com>
2024-06-11 01:37:32 +1200

    Update App.jsx

--- mission-zero-app/mission-zero-app/src/App.jsx
+++ mission-zero-app/mission-zero-app/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import NewsCard from './components/NewsCard'
 
+
 function App() {
-  const [category, setCategory] = useState ("general")
+
 
 
   return (
     <div>
-    <NavBar setCategory={setCategory}/>
-    <NewsCard category={category} />
+    <NavBar />
+    <NewsCard  />
+
     </div>
   )
 }
