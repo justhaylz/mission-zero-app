ea82e87314ecb0059b009b745c36850c93401e0a
Hayley Smith <158859106+justhaylz@users.noreply.github.com>
2024-06-11 01:37:08 +1200

    Update NavBar.jsx

--- mission-zero-app/mission-zero-app/src/components/NavBar.jsx
+++ mission-zero-app/mission-zero-app/src/components/NavBar.jsx
@@ -1,39 +1,62 @@
-import Logo from '../assets/malogo.png'
+import Logo from '../assets/malogo.png';
 
-const NavBar = ({setCategory}) => {
-	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-			<div className="container-fluid">
-			<a className="navbar-logo" href="logo"><img src={Logo} alt="logo"/></a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-			<div className="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-					<li className="nav-item"><button className="nav-link" onClick={()=>setCategory("general")}>General</button></li>
-					<li className="nav-item"><button className="nav-link" onClick={()=>setCategory("technology")}>Technology</button></li>
-					<li className="nav-item"><button className="nav-link" onClick={()=>setCategory("businrdd")}>Business</button></li>
-					<li className="nav-item"><button className="nav-link" onClick={()=>setCategory("health")}>Health</button></li>
-					<li className="nav-item"><button className="nav-link" onClick={()=>setCategory("science")}>Science</button></li>
-					<li className="nav-item"><button className="nav-link" onClick={()=>setCategory("entertainment")}>Entertainment</button></li>
-				</ul>
-			<form className="d-flex" role="search">
-				<input
-					className="form-control me-2"
-					type="search"
-					placeholder="Search"
-					aria-label="Search"/>
-				<button className="btn btn-outline-success" type="submit">Search</button>
-			</form>
-			</div>
-			</div>
-		</nav>
-	);
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <img src={Logo} alt="logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/training">
+                Training
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/events">
+                Events
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
+
