d98f125582302fd3262800069f0c575658e9d776
Hayley Smith <158859106+justhaylz@users.noreply.github.com>
2024-06-28 19:36:21 +1200

    Update NavBar.jsx

--- mission-zero-app/mission-zero-app/src/components/NavBar.jsx
+++ mission-zero-app/mission-zero-app/src/components/NavBar.jsx
@@ -1,62 +1,21 @@
 import Logo from '../assets/malogo.png';
+import styles from '../components/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <img src={Logo} alt="logo" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/services">
-                Services
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/training">
-                Training
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/events">
-                Events
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+    <nav>
+        <div className="topnav" id="myTopnav">
+          <a className={styles.navLogo}><img src={Logo} alt="logo" /></a>
+          <a className={styles.navLink} href="#">Services</a>
+          <a className={styles.navLink} href="#">Training</a>
+          <a className={styles.navLink} href="#">Events</a>
+          <a className={styles.navLink} href="#">About</a>
+          <button className={styles.navBtn}>Become a Member</button>
         </div>
-      </div>
     </nav>
   );
 };
 
 export default NavBar;
+
 
